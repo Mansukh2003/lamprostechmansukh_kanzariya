51d502f818b29989f988b82cf64b2ef0b924536f
Mansukh Kanzariya <141042831+Mansukh2003@users.noreply.github.com>
2024-01-09 17:19:20 +0530

    Add files via upload

---
+++ n-queen.py
@@ -0,0 +1,23 @@
+def fun(board, row, col, N):
+    # Check if there is a queen in the same row
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check if there is a queen in the upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check if there is a queen in the lower diagonal on the left side
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve(board, col, N, result):
+    if col == N:
+        result.append([i.index(1) + 1 for i in board])
+        return
+ 
